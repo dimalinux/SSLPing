74f24279471e8973a7f2a3f44d6a657bdc4ab68b
Дмитрий Холодов <github@sec.to>
2015-03-05 13:57:15 -0600

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # SSLPing
-Java SSL client to test for valid SSL handshake
+
+Java SSL client to test for valid SSL handshake.  I wanted a client in Java, so it verifies using
+the signing authorities trusted by your Java instance.  I also wanted a client that does not assume
+that the remote server is an HTTPS server (i.e. it can be running any protocol
+over TLS/SSL).
+
+
+##  Build the project
+```
+$ ./gradlew clean build
+```
+
+## Example Use
+
+### Run against a known good certificate
+```
+$ java -jar build/libs/SSLPing.jar www.google.com 443
+About to connect to 'www.google.com' on port 443
+Successfully connected
+```
+
+### Run against a known bad certificate
+```
+$ java -jar build/libs/SSLPing.jar testssl-expire.disig.sk 443
+javax.net.ssl.SSLHandshakeException: sun.security.validator.ValidatorException:
+    PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException:
+        unable to find valid certification path to requested target
+	at sun.security.ssl.Alerts.getSSLException(Alerts.java:192)
+...
+```
+
+### Run against a server port that's not using SSL
+```
+$ java -jar build/libs/SSLPing.jar www.yahoo.com 80
+About to connect to 'www.yahoo.com' on port 80
+javax.net.ssl.SSLException: Unrecognized SSL message, plaintext connection?
+```
---
+++ src/main/java/to.noc.sslping/SSLPing.java
@@ -0,0 +1,45 @@
+package to.noc.sslping;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+/*
+ *  Simple tool to test an SSL handshake to a remote server that does not assume that
+ *  the remote end is an HTTP(S) server.
+ */
+public class SSLPing {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: " + SSLPing.class.getName() + " <host> <port>");
+            System.exit(1);
+        }
+        try {
+
+            String hostname = args[0];
+            int port = Integer.parseInt(args[1]);
+
+            System.out.println("About to connect to '" + hostname + "' on port " + port);
+
+            SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket(args[0], Integer.parseInt(args[1]));
+
+            InputStream in = sslsocket.getInputStream();
+            OutputStream out = sslsocket.getOutputStream();
+
+            // Write a test byte to get a reaction :)
+            out.write(1);
+
+            while (in.available() > 0) {
+                System.out.print(in.read());
+            }
+            System.out.println("Successfully connected");
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
